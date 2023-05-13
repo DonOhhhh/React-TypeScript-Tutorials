7152e940501545ca40c17651cfacb08044d9925d
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-05-13 18:38:57 +0900

    useState Type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,22 +6,20 @@ type AuthUser = {
 };
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null);
+    // 언제나 component가 마운트될 때 user 정보가 들어올 것이 확실하다면 as 문법을 통해 빈 object를 AuthUser로 assert할 수 있다.
+    const [user, setUser] = useState<AuthUser>({} as AuthUser);
     const handleLogin = () => {
         setUser({
             name: "Dong Joon",
             email: "1234@gmail.com",
         });
     };
-    const handleLogout = () => {
-        setUser(null);
-    };
+
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     );
 };
